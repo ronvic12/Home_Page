b50d5895644cf396eecd9ce4aa37dba8a9128578
Eric Ngo <epngo@ucdavis.edu>
2021-06-08 01:46:33 -0700

    Simple Home Screen (6/8 edit)

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,72 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "App",
-      home: new Scaffold(
-        appBar: new AppBar(title: new Text("Home Page")),
-        body: new Center(
-          child: new Text("Run my bag"),
-        ),
+      title: 'An OpportunityKnocks App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: HomePage(),
     );
   }
 }
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 60),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              IconButton(icon: Icon(Icons.notifications), onPressed: () {}),
+            ],
+          ),
+          SizedBox(height: 30),
+          TextField(
+            decoration: InputDecoration(
+                prefixIcon: Icon(Icons.search, size: 18),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                hintText: 'Search'),
+          ),
+          SizedBox(height: 30),
+          Text('All Benefits',
+              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20)),
+          SizedBox(height: 10),
+          SizedBox(
+            height: 300,
+            child: GridView.count(
+                padding: EdgeInsets.zero,
+                crossAxisCount: 2,
+                childAspectRatio: 1.491,
+                physics: NeverScrollableScrollPhysics(),
+                children: [
+                  for (var benefit in benefits)
+                    Image.asset('assets/images/benefits/$benefit.png')
+                ]),
+          )
+        ],
+      ),
+    ));
+  }
+}
+
+final benefits = [
+  'Ali_Baba',
+  'Davis_Creamery',
+  'UC_Davis_Football',
+  'Sac_Kings'
+];
